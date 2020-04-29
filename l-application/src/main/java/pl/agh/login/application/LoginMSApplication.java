4991395dbf0da7997b92579ce09317ef0932fc83
rxrt8 <michal25kos@gmail.com>
2020-04-29 19:13:44 +0200

    add eureka client

--- l-application/src/main/java/pl/agh/login/application/LoginMSApplication.java
+++ l-application/src/main/java/pl/agh/login/application/LoginMSApplication.java
@@ -2,6 +2,7 @@ package pl.agh.login.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.ComponentScan;
                 "pl.agh.login"
         }
 )
+@EnableEurekaClient
 public class LoginMSApplication {
 
     public static void main(String[] args) {
